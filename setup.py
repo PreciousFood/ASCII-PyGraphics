109e73936100eb194aca19b64d6444f320c2a0b0
Leo <preciousfood84@gmail.com>
2023-12-02 20:50:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ASCIIGraphics", 
-    version="1.0.3", 
+    version="0.0.1", 
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -16,34 +16,4 @@ setup(
     url="https://github.com/PreciousFood/ASCIIGraphics"
 )
 
-# install setup stuffs...
-# pip install setuptools wheel twine
-# Ensure that it is this structure
-# PROJECT_NAME/
-# ├── PROJECT_NAME/
-# │   ├── __init__.py
-# │   └── main.py
-# ├── setup.py
-# ├── LICENSE.txt
-# └── README.md
-#
-# In __init__.py
-#   from .main import [stuff]
-# 
-# Build stuff
-# python setup.py sdist bdist_wheel
-#
-#
-# COMMANDS FOR TESTING (Local)
-# pip install dist\ASCIIGraphics-1.0.0-py3-none-any.whl --force-reinstall
-# pip uninstall ASCIIGraphics
-# pip list
-#
-#
-# Upload time!!!
-# twine upload dist/*
-# twine upload --repository testpypi dist/*
-#
-#
-# MORE TESTING!
-# pip install PROJECT_NAME
+
